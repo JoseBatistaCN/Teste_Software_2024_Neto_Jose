e5b657c2cefee36b571b9dac5ced300f94346d27
Jose Batista <batistdev@gmail.com>
2024-08-06 22:34:27 -0300

    refactor: alterado inicialização da lista de lances

--- Teste_Software_2024_Neto_Jose/Leilao.cs
+++ Teste_Software_2024_Neto_Jose/Leilao.cs
@@ -2,11 +2,11 @@
 {
     public class Leilao
     {
-        public IList<Lance> Lances { get; private set; }
+        public List<Lance> Lances { get; private set; }
 
         public Leilao()
         {
-            Lances = [];
+            Lances = new List<Lance>();
         }
 
         public void RecebeLance(string nomeDoUsuario, double valor)
